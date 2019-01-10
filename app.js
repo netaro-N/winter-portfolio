1e3ac6c88ebe46d5914a0d90c2e804a860925414
netaro-N <takebbb1+github@gmail.com>
2019-01-10 05:49:47 +0000

    ツイッターの認証機能を実装完了！

--- app.js
+++ app.js
@@ -19,9 +19,13 @@ var helmet = require('helmet');
 var session = require('express-session');
 var passport = require('passport');
 var GitHubStrategy = require('passport-github2').Strategy;
+var TwitterStrategy = require('passport-twitter').Strategy;
 
-var GITHUB_CLIENT_ID = '1a0659b7b108e8fd9c2d';
-var GITHUB_CLIENT_SECRET = 'a1e75c1028678718afa83aff470212761a3cb3a5';
+var GITHUB_CLIENT_ID = '6d52bc06fd83ea97a35a';
+var GITHUB_CLIENT_SECRET = 'a7e841c7358243dc938fd222e62ce91b227b3119';
+
+var TWITTER_CONSUMER_KEY = 'S8S8vfWzTpDOmc64t7cbgcyVh';
+var TWITTER_CONSUMER_SECRET = 'UN1yXfQX88wT70JDbnKYIViAg9uWWhWL03kJZuHaqKp8XzTPl3';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -31,16 +35,32 @@ passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
+// GitHubの認証設定
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback'
+  callbackURL: 'http://example.net:8000/auth/github/callback'
 },
+// 認証後アクション
   function (accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
       return done(null, profile);
     });
   }
+));
+
+// Twitterの認証設定
+passport.use(new TwitterStrategy({
+  consumerKey: TWITTER_CONSUMER_KEY,
+  consumerSecret: TWITTER_CONSUMER_SECRET,
+  callbackURL: "http://example.net:8000/auth/twitter/callback"
+},
+// 認証後アクション
+function (accessToken, refreshToken, profile, done) {
+  process.nextTick(function () {
+    return done(null, profile);
+  });
+}
 ));
 
 var indexRouter = require('./routes/index');
@@ -68,16 +88,27 @@ app.use('/', indexRouter);
 app.use('/login',loginRouter);
 app.use('/logout', logoutRouter);
 
+// GitHub認証のハンドラ
 app.get('/auth/github',
   passport.authenticate('github', { scope: ['user:email'] }),
   function (req, res) {
 });
-
+// GitHub が利用者の許可に対する問い合わせの結果を送るパスハンドラの登録
 app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
   function (req, res) {
     res.redirect('/');
 });
+
+// Twitter認証のハンドラ
+app.get('/auth/twitter',
+ passport.authenticate('twitter'),
+ function (req, res) {
+});
+// callbackのハンドラ
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { successRedirect: '/',
+                                     failureRedirect: '/login' }));
 
 app.get('/login', function (req, res) {
   res.render('login');
