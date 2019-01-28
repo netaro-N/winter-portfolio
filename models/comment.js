e65e65443f63c9b01ca549538dd978c30b9776b2
netaro-N <takebbb1+github@gmail.com>
2019-01-28 04:44:07 +0000

    database

---
+++ models/comment.js
@@ -0,0 +1,30 @@
+'use strict';
+const loader = require('./sequelize-loader');
+const Sequelize = loader.Sequelize;
+
+const Comment = loader.database.define('comments', {
+  fixtureId: {
+    type:Sequelize.UUID,
+    primaryKey: true,
+    allowNull: false
+  },
+  userId: {
+    type: Sequelize.BIGINT,
+    primaryKey: true,
+    allowNull: false
+  },
+  userProvider: {
+    type: Sequelize.STRING,
+    primaryKey:true,
+    allowNull: false
+  },
+  comment: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+}, {
+  freezeTableName: true,
+  timestamps: false
+});
+
+module.exports = Comment;
--- models/fixture.js
+++ models/fixture.js
@@ -3,16 +3,20 @@ const loader = require('./sequelize-loader');
 const Sequelize = loader.Sequelize;
 
 const Fixture = loader.database.define('fixtures', {
-  fixtureDate: {//20190129
-    type: Sequelize.DATE,
+  fixtureId: {//連番
+    type: Sequelize.INTEGER,
     primaryKey: true,
+    autoIncrement:true,
     allowNull: false
   },
-  fixture: {//madrid-vs-atmadrid
+  fixtureDate: {//20190129
+    type: Sequelize.DATE,
+  },
+  fixtureName: {//madrid-vs-atmadrid
     type: Sequelize.STRING,
     allowNull: false
   },
-  resultSain:{
+  fixtureDone:{//先の試合(0) or 次の（今の）試合(1) or 終了した試合(2)
     type: Sequelize.INTEGER,
     allowNull: false,
     defaultValue: 0
