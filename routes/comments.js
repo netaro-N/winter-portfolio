4c1f1ccf4bb382704d5f4e116101e45c7066a5a2
netaro-N <takebbb1+github@gmail.com>
2019-01-29 11:22:12 +0000

    comments.js

---
+++ routes/comments.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const authenticationEnsurer = require('./authentication-ensurer');
+
+router.post('/', authenticationEnsurer, (req, res, next) => {
+  console.log(req.body); // コメントを保存する実装をする
+  res.redirect('/index');
+});
+
+module.exports = router;
