e6e6d470e1680a0a5988f7913da1e7282c634145
netaro-N <takebbb1+github@gmail.com>
2019-02-01 10:49:06 +0000

    use uuid

--- routes/fixtures.js
+++ routes/fixtures.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const authenticationEnsurer = require('./authentication-ensurer');
+const uuid = require('uuid');
 const Fixture = require('../models/fixture');
